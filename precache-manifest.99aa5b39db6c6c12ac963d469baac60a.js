380d72903bf38909eb2ba8f2605bf93eb7095b9d
unknown <yasodatalapu227@gmail.com>
2021-07-04 17:45:29 +0530

    Updates

---
+++ precache-manifest.99aa5b39db6c6c12ac963d469baac60a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d67e25efecdccd82959757b4e1d660",
+    "url": "/Vocab_app/index.html"
+  },
+  {
+    "revision": "536b7b77ee3eacb5ae57",
+    "url": "/Vocab_app/static/js/2.909eb0f1.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/Vocab_app/static/js/2.909eb0f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33e96a78424f79a4265",
+    "url": "/Vocab_app/static/js/main.d3cfcd42.chunk.js"
+  },
+  {
+    "revision": "718574abaeed4e78fa2f",
+    "url": "/Vocab_app/static/js/runtime-main.ad687f4a.js"
+  }
+]);
